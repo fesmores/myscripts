6ce17c856603ffc2c826dbebe2cec67c1a5ef818
fesmores <franco.esmores@gmail.com>
2019-03-25 14:29:59 -0300

    Second commit - to brandnewbranch

--- cotizacion.sh
+++ cotizacion.sh
@@ -7,4 +7,5 @@ fecha=$(date "+%d-%m-%Y %H:%M")
 echo -e "\n\033[1;32m Cotizacion compra Billete Dolar USA Banco Nacion \033[1;33m $ ${compra} \033[1;32mpesos - Fecha ${fecha} \033[1;0m\n"
 echo -e "\n\033[1;32m Cotizacion venta Billete Dolar USA Banco Nacion \033[1;31m $ ${venta} \033[1;32mpesos - Fecha ${fecha} \033[1;0m\n"
 
+echo "new push to brandnewbranch"
 
